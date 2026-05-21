03eacfa18a058013dc367f79e7efb7cf47a136c0
Your Name <you@example.com>
2026-05-21 10:38:02 +0300

    New data moving instruction schema implemented except in emulator

--- src/compiler/entry.rs
+++ src/compiler/entry.rs
@@ -1,7 +1,7 @@
 use std::io::Write;
 use num_bigint::BigUint;
 use num_traits::{ToBytes, ToPrimitive};
-use crate::compiler::operand_checking::MOV_SIGNATURE;
+use crate::compiler::operand_checking::{COPY_SIGNATURE, EXT_SIGNATURE, MOV_SIGNATURE, TRUNC_SIGNATURE};
 use crate::compiler::operands::{parse_operands, parse_u64_num, Operand, OperandKind};
 use crate::compiler::ParseError;
 
@@ -22,19 +22,6 @@ pub fn entry(file: &str) {
     file.write_all(&output).unwrap();
 }
 
-// TODO: make it work with new operands system
-///Moving stuff around
-///
-/// Endianness: BE
-///
-/// |           Dest            |                            Address (RAM)                             | Immediate (1 byte) |    LongImmediate (8 bytes)    |   LongerImmediate (9 bytes)   | Register | LongRegister |
-/// | :-----------------------: | :------------------------------------------------------------------: | :----------------: | :---------------------------: | :---------------------------: | :------: | :----------: |
-/// |       Address (RAM)       |                 copy (with specific number of bytes)                 |     mov (addr)     |          mov (addr+)          |         mov  (addr+)          |   mov    | mov  (addr+) |
-/// |    Immediate (1 byte)     |                                 N/A                                  |        N/A         |              N/A              |              N/A              |   N/A    |     N/A      |
-/// |  LongImmediate (8 bytes)  |                                 N/A                                  |        N/A         |              N/A              |              N/A              |   N/A    |     N/A      |
-/// | LongerImmediate (9 bytes) |                                 N/A                                  |        N/A         |              N/A              |              N/A              |   N/A    |     N/A      |
-/// |         Register          |                   mov (moves byte att addr to reg)                   |        mov         | trunc (low byte but like why) | trunc (low byte but like why) |   mov    |    trunc     |
-/// |       LongRegister        | copy (for 8 byte seq starting with addr) / ext (for 1 bytes at addr) |        ext         |              mov              |             trunc             |   ext    |     mov      |
 pub fn compile(line: &str) -> Vec<u8> {
     let parts = line.split(" ").collect::<Vec<&str>>();
     if parts.len() == 0 {
@@ -42,8 +29,12 @@ pub fn compile(line: &str) -> Vec<u8> {
     }
     let op = parts[0];
     let args = &parts[1..];
-
+    
+    // TODO: refactor bc of repetition
     match op {
+        "hlt" => {  // Stop execution and enter endless loop
+            vec![0x00]
+        }
         "mov" => {  // instruction to move value to specified address in memory
             let result = parse_operands(args);
             let (kinds, operands) = match result {
@@ -51,154 +42,101 @@ pub fn compile(line: &str) -> Vec<u8> {
                     (kinds, operands)
                 }
                 Err(e) => {
-                    println!("Failed parsing operands: {:?}. Line is gonna be treated as noop", e);
+                    println!("Failed parsing operands: {:?}. Line {} is gonna be treated as noop", e, line);
                     return vec![0];
                 }
             };
 
             if !MOV_SIGNATURE.check(&kinds){
-                if kinds == vec![OperandKind::Immediate, OperandKind::LongRegister] {
-                    println!("Incorrect operands: You can't load immediate into long register with `mov` instruction.\nYou probably meant to use `movl` to load 8-byte immediate or `load` to load any other side")
-                }
-                else if kinds.iter().any(|x| *x == OperandKind::LongRegister) {
-                    println!("Incorrect operands: You can't manipulate long registers with `mov` instruction.\nTry `load` for non 8-length immediate values or `movl` for everything else\n{} is gonna be treated as noop", line);
-                } else {
-                    println!("Incorrect operands\n{} is gonna be treated as noop", line);
-                }
+                // TODO: add hints
+                println!("Incorrect operands\n{} is gonna be treated as noop", line);
                 return vec![0];
             }
 
-            todo!()
-        }
-        "movl" => {  // 2
-            if args.len() != 2 {
-                println!("Wrong number of arguments for mov. It's gonna be treated as noop");
-                return vec![0]
-            };
+            let mut output = vec![0x10];
 
-            let arg1 = match parse_operand(args[0].to_string()) {
-                Ok(v) => v,
+            for operand in operands {
+                output.extend(operand.to_bytes());
+            }
+            
+            output
+        }
+        "trunc" => {
+            let result = parse_operands(args);
+            let (kinds, operands) = match result {
+                Ok((kinds, operands)) => {
+                    (kinds, operands)
+                }
                 Err(e) => {
-                    println!("Parse error: {:?}. Treating as noop", e);
+                    println!("Failed parsing operands: {:?}. Line {} is gonna be treated as noop", e, line);
                     return vec![0];
                 }
             };
-            let arg2 = match parse_operand(args[1].to_string()) {
-                Ok(v) => v,
+
+            if !TRUNC_SIGNATURE.check(&kinds){
+                // TODO: add hints
+                println!("Incorrect operands\n{} is gonna be treated as noop", line);
+                return vec![0];
+            }
+
+            let mut output = vec![0x11];
+
+            for operand in operands {
+                output.extend(operand.to_bytes());
+            }
+
+            output
+        }
+        "ext" => {
+            let result = parse_operands(args);
+            let (kinds, operands) = match result {
+                Ok((kinds, operands)) => {
+                    (kinds, operands)
+                }
                 Err(e) => {
-                    println!("Parse error: {:?}. Treating as noop", e);
+                    println!("Failed parsing operands: {:?}. Line {} is gonna be treated as noop", e, line);
                     return vec![0];
                 }
             };
 
-            match (arg1, arg2) {
-                (Operand::Immediate(v), Operand::LongRegister(lr)) => {
-                    let mut s = v.to_be_bytes();
-                    if s.len() > 8 {
-                        println!("Amount of bytes isn't corresponding to immediate value size. Line's gonna be treated as noop");
-                        return vec![0]
-                    }
-                    if s.len() < 8 {
-                        let mut padding = vec![0u8; 8 - s.len()];
-                        padding.extend_from_slice(s.as_slice());
-                        s = padding;
-                    }
-
-                    let mut output = vec![];
-                    output.push(2);
-                    output.push(0);
-                    output.extend(s);
-                    output.push(lr.to_bytecode());
-                    output
-                }
-                (Operand::LongRegister(rl), Operand::Address(addr)) => {
-                    let mut output = vec![];
-                    output.push(2);
-                    output.push(1);
-                    output.push(rl.to_bytecode());
-                    output.extend(addr.to_be_bytes().to_vec());
-                    output
-                }
-                (Operand::LongRegister(rl1), Operand::LongRegister(rl2)) => {
-                    let mut output = vec![];
-                    output.push(2);
-                    output.push(2);
-                    output.push(rl1.to_bytecode());
-                    output.push(rl2.to_bytecode());
-                    output
-                }
-                (_, _) => {
-                    println!("Unknown/Illegal combination of operands for movl. Line's gonna be treated as noop");
-                    vec![0]
-                }
-            }
-        }
-        "hlt" => {  // Stop execution and enter endless loop
-            vec![3]
-        }
-        "load" => {
-            if args.len() != 3 {
-                println!("Wrong number of arguments for load. It's gonna be treated as noop");
-                return vec![0]
+            if !EXT_SIGNATURE.check(&kinds){
+                // TODO: add hints
+                println!("Incorrect operands\n{} is gonna be treated as noop", line);
+                return vec![0];
             }
 
-            let n = match args[0].parse::<u8>() {
-                Ok(v) => {
-                    if v > 8 {
-                        println!("Limit for amount of bytes to load is 8. Line's gonna be treated as noop");
-                        return vec![0]
-                    }
+            let mut output = vec![0x12];
 
-                    v
-                },
+            for operand in operands {
+                output.extend(operand.to_bytes());
+            }
+
+            output
+        }
+        "copy" => {
+            let result = parse_operands(args);
+            let (kinds, operands) = match result {
+                Ok((kinds, operands)) => {
+                    (kinds, operands)
+                }
                 Err(e) => {
-                    println!("Limit for amount of bytes to load is 8. Line's gonna be treated as noop");
-                    return vec![0]
+                    println!("Failed parsing operands: {:?}. Line {} is gonna be treated as noop", e, line);
+                    return vec![0];
                 }
             };
 
-            let value = match parse_operand(args[1].to_string()) {
-                Ok(v) => {
-                    match v {
-                        Operand::Immediate(imm) => {
-                            let mut s = imm.to_be_bytes();
-                            if s.len() > n as usize {
-                                println!("Amount of bytes isn't corresponding to immediate value size. Line's gonna be treated as noop");
-                                return vec![0]
-                            }
-                            if s.len() < n as usize {
-                                let mut padding = vec![0u8; n as usize - s.len()];
-                                padding.extend_from_slice(s.as_slice());
-                                s = padding;
-                            }
-                            s
-                        }
-                        _ => {
-                            println!("Second argument should be an immediate value. Line's gonna be treated as noop");
-                            return vec![0]
-                        }
-                    }
-                }
-                Err(e) => {
-                    println!("Error parsing immediate value");
-                    return vec![0]
-                }
-            };
+            if !COPY_SIGNATURE.check(&kinds){
+                // TODO: add hints
+                println!("Incorrect operands\n{} is gonna be treated as noop", line);
+                return vec![0];
+            }
 
-            let addr = match parse_u64_num(args[2].to_string()) {
-                Ok(v) => v,
-                Err(e) => {
-                    println!("Third argument should be an address. Line's gonna be treated as noop");
-                    return vec![0]
-                }
-            };
+            let mut output = vec![0x13];
 
-            let mut output = vec![];
-            output.push(4);
-            output.push(0);
-            output.push(n);
-            output.extend(value);
-            output.extend(addr.to_be_bytes().to_vec());
+            for operand in operands {
+                output.extend(operand.to_bytes());
+            }
+
             output
         }
         _ => {
